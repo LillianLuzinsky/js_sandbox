4c7a9b73f232ad3845de2bc3bc0ec496c4512091
Lillian Luzinsky <eyeris@tpg.com.au>
2020-05-01 15:01:40 +1000

    using the console

---
+++ app.js
@@ -0,0 +1,21 @@
+// Log to console
+console.log('Hello world');
+console.log(123);
+console.log(true);
+let greeting = 'hello';
+console.log(greeting);
+console.log([1,2,3,4]);
+console.table({a:1, b:2, c:3});
+
+console.error('This is some error');
+console.clear();// clears everything
+console.warn('this is a warning');
+
+console.time('hello');
+    console.log('hello');
+    console.log('hello');
+    console.log('hello');
+    console.log('hello');
+    console.log('hello');
+    console.log('hello');
+console.timeEnd('Hello world');
