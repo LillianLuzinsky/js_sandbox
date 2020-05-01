029ecd81fe833d2881a25b836ec9b3cb50ab588b
Lillian Luzinsky <eyeris@tpg.com.au>
2020-05-01 16:57:53 +1000

    using Numbers and Math objects

--- app.js
+++ app.js
@@ -113,43 +113,43 @@ var FirstName = 'tom';  //pascal case used for constructor function
 // console.log(typeof today); //object
 
 // --------TYPE CONVERSION --------
-let val;
+// let val;
 
-// number to string
-val = 5;
-val = String(5); // length = 1
-val = String(4+8); // length = 2 
+// // number to string
+// val = 5;
+// val = String(5); // length = 1
+// val = String(4+8); // length = 2 
 
-//Boolean to a string
-val = String(true); // length = 4
+// //Boolean to a string
+// val = String(true); // length = 4
 
-//Date to string
-val= String(new Date()); // writes the entire date. 
-                        // length = 68
+// //Date to string
+// val= String(new Date()); // writes the entire date. 
+//                         // length = 68
 
-//Array to string
-val = String([1,2,3,4]); // 1,2,3,4     length=7 (int and commas)
+// //Array to string
+// val = String([1,2,3,4]); // 1,2,3,4     length=7 (int and commas)
 
-//toString()
-val = (5).toString(); // acts the same as String(5)
-val = (true).toString(); //same as String(true);
-val = (new Date()).toString(); //same as String(new Date());
+// //toString()
+// val = (5).toString(); // acts the same as String(5)
+// val = (true).toString(); //same as String(true);
+// val = (new Date()).toString(); //same as String(new Date());
 
-// String to numbers
-val = Number('5');// toFixed 5.00
+// // String to numbers
+// val = Number('5');// toFixed 5.00
 
-// Boolean to number
-val = Number(true);// toFixed 1
-val = Number(false);// toFixed 0
-val = Number(null); // toFixed 0
-val = Number('hello')// toFixed NaN
-val = Number([1,2,3,4])// toFixed NaN
+// // Boolean to number
+// val = Number(true);// toFixed 1
+// val = Number(false);// toFixed 0
+// val = Number(null); // toFixed 0
+// val = Number('hello')// toFixed NaN
+// val = Number([1,2,3,4])// toFixed NaN
 
-//parseInt
-val = parseInt('100')//100
-val = parseFloat('100.20')// toFixed 100.20
-val = parseInt('hello')// NaN
-val = parseInt(false);// NaN
+// //parseInt
+// val = parseInt('100')//100
+// val = parseFloat('100.20')// toFixed 100.20
+// val = parseInt('hello')// NaN
+// val = parseInt(false);// NaN
 
 // console.log(val);
 // console.log(typeof val);
@@ -157,10 +157,44 @@ val = parseInt(false);// NaN
 // console.log(val.toFixed(2));
 
 // type cohersion
-val1 = '5';
-val2 = 6;
-sum = Number(val1 + val2); //number
+// val1 = '5';
+// val2 = 6;
+// sum = Number(val1 + val2); //number
 
-console.log(sum); //56
-console.log(typeof sum); //string
+// console.log(sum); //56
+// console.log(typeof sum); //string
 
+
+
+//-------Numbers and the Math Object ------
+
+const num1 = 100;
+const num2 = 50;
+let val;
+
+//simple math with numbers
+val = num1 + num2; // 150
+val = num1 * num2; // 5000
+val = num1 - num2; // 50
+val = num1 / num2; // 2
+val = num1 % num2; // 0
+
+// Math object
+val = Math.PI;//3.141592653589793
+val = Math.E;//2.718281828459045
+val = Math.round(2.8);//3
+val = Math.round(2.4);//2
+val = Math.ceil(2.4);//3
+val = Math.floor(2.8);//2
+val = Math.sqrt(64)//8
+val = Math.abs(-3);//3
+val = Math.pow(8, 2); //64
+val = Math.min(2, 33, 4, 1, 55, 3, -2); //-2 
+val = Math.max(2, 33, 4, 1, 55, 3, -2); //55
+
+val = Math.random(2, 33, 4, 1, 55, 3, -2); //0.xxxxx
+val = Math.random() * 20; // decimal
+val = Math.floor(Math.random() * 20 + 1); // whole numbers excl 0
+
+
+console.log(val);
