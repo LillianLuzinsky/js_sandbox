e4a191607f03548a22ca51b61ac55ba15c247f86
Lillian Luzinsky <eyeris@tpg.com.au>
2020-05-15 14:45:01 +1000

    practicing switch

--- app.js
+++ app.js
@@ -510,3 +510,53 @@
 
 // //TERNARY OPERATOR
 // console.log(id === 100 ? 'correct' : 'incorrect');
+
+
+//---------SWITCHES---------
+// const colour = 'yellow';
+
+// switch(colour){
+//     case 'red':
+//         console.log('colour is red');
+//         break;
+//     case 'blue':
+//         console.log('colour is blue');
+//         break;
+//     default:
+//         console.log('colour is not red or blue')
+//         break;
+// }
+
+// let day;
+
+// switch(new Date().getDay()){
+//     case 0: 
+//     day = 'Sunday';
+//     break;
+
+//     case 1: 
+//     day = 'Monday';
+//     break;
+
+//     case 2: 
+//     day = 'Tuesday';
+//     break;
+
+//     case 3: 
+//     day = 'Wednesday';
+//     break;
+
+//     case 4: 
+//     day = 'Thursday';
+//     break;
+
+//     case 5: 
+//     day = 'Friday';
+//     break;
+
+//     case 6: 
+//     day = 'Saturday';
+//     break;
+// }
+
+// console.log(`today is ${day}`)//today is Friday
